ae7080f071cfb3226c64954a469e7056e04937fd
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2024-02-26 20:00:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function validEmail(str) {
   //your JS code here.
+	let pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+    return pattern.test(email)
 }
 
 // Do not change the code below.
